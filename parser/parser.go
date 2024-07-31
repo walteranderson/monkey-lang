35321c5c34bcac4e20285af192d3ab16500fc580
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-07-31 09:05:33 -0500

    chapter 2.5 parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -80,7 +82,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO: skipping expressions until we encounter a semicolon
-	if !p.curTokenIs(token.SEMICOLON) {
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: skipping expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
