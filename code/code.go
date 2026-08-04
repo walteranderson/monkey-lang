1fd0f7c7eea9a6903f7f604678090215446cccec
Walter Anderson <walteranderson19@gmail.com>
2026-08-04 10:23:25 -0500

    OpIndex

--- code/code.go
+++ code/code.go
@@ -61,6 +61,7 @@ const (
 	OpGetGlobal
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -89,6 +90,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpHash", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
